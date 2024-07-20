83e275288df648501dac5e1bccb6fec4c6808b4e
Prabin1025y <acharyaprabin1025y@gmail.com>
2024-07-20 22:10:52 +0545

    responsiveness improved

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -143,30 +143,32 @@ const Manager = () => {
 			<ToastContainer pauseOnFocusLoss={false} />
 			<div className="absolute inset-0 -z-10 h-full w-full bg-sky-50 bg-[linear-gradient(to_right,#0ea5e90a_1px,transparent_1px),linear-gradient(to_bottom,#0ea5e90a_1px,transparent_1px)] bg-[size:14px_24px]">{/* <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-sky-500 opacity-20 blur-[100px]"></div> */}</div>
 			<div className=" container mx-auto p-10 flex flex-col items-center">
-				<h1 className="text-3xl font-bold">
+				<h1 className=" text-3xl font-bold">
 					<span className="text-sky-500">&#123;</span> Pass<span className="text-sky-500">Guard &#125;</span>
 				</h1>
 				<h3 className="text-sky-500">Where Security Meets Simplicity</h3>
-				<form className="flex flex-col gap-4 w-1/2 my-8 items-center" action="">
+				<form className="flex flex-col gap-4 w-full md:w-11/12 lg:w-1/2 my-8 items-center" action="">
 					<section className="w-full">
-						<input required value={userData.sitename} onChange={handleChange} name="sitename" placeholder="Enter The Website..." className="rounded-full p-1 text-sky-800 focus-visible:outline-sky-700 border border-sky-500 w-full" type="text" />
+						<input required value={userData.sitename} onChange={handleChange} name="sitename" placeholder="Enter The Website..." className="rounded-full p-1 text-sky-800 focus-visible:outline-sky-700 border border-sky-500 w-full text-sm lg:text-lg" type="text" />
 					</section>
 					<section className="flex gap-12 w-full">
-						<input required value={userData.username} onChange={handleChange} name="username" placeholder="Enter Username" className="border p-1 focus-visible:outline-sky-700 text-sky-800 focus:border-sky-700 border-sky-500 rounded-full w-full" type="text" />
+						<input required value={userData.username} onChange={handleChange} name="username" placeholder="Enter Username" className="border p-1 focus-visible:outline-sky-700 text-sky-800 focus:border-sky-700 border-sky-500 rounded-full w-full text-sm lg:text-lg" type="text" />
 						<div className="w-full  relative">
-							<input required value={userData.password} onChange={handleChange} name="password" ref={passwordInputRef} placeholder="Enter Password" className="border p-1 focus-visible:outline-sky-700 text-sky-800 focus:border-sky-700 border-sky-500 rounded-full w-full" type="password" />
+							<input required value={userData.password} onChange={handleChange} name="password" ref={passwordInputRef} placeholder="Enter Password" className="border p-1 focus-visible:outline-sky-700 text-sky-800 focus:border-sky-700 border-sky-500 rounded-full w-full text-sm lg:text-lg" type="password" />
 							<img ref={visibleRef} className="absolute top-[5px] right-[8px] cursor-pointer" onClick={handleToogleVisible} src="svgs/notVisible.svg" alt="" />
 						</div>
 					</section>
-					<button onClick={handleAddData} className="bg-sky-500 text-white rounded-xl hover:bg-sky-600 transition-colors duration-400 p-3 w-fit flex" type="submit">
+					<button onClick={handleAddData} className="bg-sky-500 text-white rounded-xl hover:bg-sky-600 transition-colors duration-400 p-2 w-fit flex items-center text-sm lg:text-lg" type="submit">
 						{/* <img src="Gifs/add-file.gif" alt="" /> */}
-						<lord-icon src="https://cdn.lordicon.com/jgnvfzqg.json" trigger="hover" colors="primary:#ffffff" style={{ width: "25px", height: "25px" }}></lord-icon>
-						SAve
+						{/* <lord-icon src="https://cdn.lordicon.com/jgnvfzqg.json" trigger="hover" colors="primary:#ffffff" className="w-[2px] h-[2px]"></lord-icon> */}
+						<img src="svgs/save.svg" alt="save" className="mr-1 size-4 lg:size-5" />
+						Save
 					</button>
 				</form>
 				{users.length === 0 && <div>No Passwords Saved.</div>}
-				{users.length !== 0 &&
-					<table className="min-w-full bg-sky-100">
+				{users.length !== 0 && <div className="w-full">
+
+					<table className="min-w-full bg-sky-100 hidden">
 						<thead>
 							<tr className="bg-sky-800 text-white">
 								<th className="px-6 py-3 text-left text-sm leading-4 uppercase tracking-wider">Site</th>
@@ -178,15 +180,30 @@ const Manager = () => {
 						<tbody>
 							{users.map((item, index) => {
 								return <tr key={index}>
-									<td className="px-6 py-4 border-b border-sky-700">{item.sitename}</td>
-									<td className="px-6 py-4 border-b border-sky-700"><div onClick={() => copyText(item.username)} className="flex gap-2">{item.username}<img className="w-5 cursor-pointer" src="svgs/copy.svg" alt="copy Text" /></div></td>
-									<td className="px-6 py-4 border-b border-sky-700"><div onClick={() => copyText(item.password)} className="flex gap-2">{item.password}<img className="w-5 cursor-pointer" src="svgs/copy.svg" alt="copy Text" /></div></td>
-									<td className="px-6 py-4 border-b border-sky-700"><div className="flex gap-3"><img className="cursor-pointer" onClick={() => handleEdit(item.id)} src="svgs/edit.svg" alt="Edit" /><img className="cursor-pointer" onClick={() => handleDelete(item.id)} src="svgs/delete.svg" alt="Delete" /></div></td>
+									<td className="text-sm lg:text-lg px-6 py-4 border-b border-sky-700">{item.sitename}</td>
+									<td className="text-sm lg:text-lg px-6 py-4 border-b border-sky-700"><div className="flex gap-2">{item.username}<img onClick={() => copyText(item.username)} className="w-5 cursor-pointer" src="svgs/copy.svg" alt="copy Text" /></div></td>
+									<td className="text-sm lg:text-lg px-6 py-4 border-b border-sky-700"><div className="flex gap-2">{item.password}<img onClick={() => copyText(item.username)} className="w-5 cursor-pointer" src="svgs/copy.svg" alt="copy Text" /></div></td>
+									<td className="text-sm lg:text-lg px-6 py-4 border-b border-sky-700"><div className="flex gap-3"><img className="cursor-pointer" onClick={() => handleEdit(item.id)} src="svgs/edit.svg" alt="Edit" /><img className="cursor-pointer" onClick={() => handleDelete(item.id)} src="svgs/delete.svg" alt="Delete" /></div></td>
 								</tr>
 							})}
 
 						</tbody>
-					</table>}
+					</table>
+					<section>
+						<div className="bg-sky-100 overflow-hidden rounded-xl">
+							<h2 className="bg-sky-800 text-white px-3 py-1">SiteNAme</h2>
+							<div className="p-1 pl-8">
+								<p>username:dkhflskdj</p>
+								<p>password:dkhflskdj</p>
+								<div className="flex gap-3 text-lg">
+									<img className="cursor-pointer" onClick={() => handleEdit(item.id)} src="svgs/edit.svg" alt="Edit" />
+									<img className="cursor-pointer" onClick={() => handleDelete(item.id)} src="svgs/delete.svg" alt="Delete" />
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+				}
 			</div>
 		</>
 	);
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,19 +2,13 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className="flex items-center bg-[#09182e] text-white h-14 justify-between px-32">
-			<h1 className="text-2xl font-bold">
+		<nav className="flex items-center bg-[#09182e] text-white h-14 justify-between px-6 md:px-32">
+			<h1 className="text-xl md:text-2xl font-bold">
 				<span className="text-sky-500">&#123;</span> Pass<span className="text-sky-500">Guard &#125;</span>
 			</h1>
 			<div className="flex gap-14">
 				<a className="hover:text-sky-300 transition-all duration-400 ease-linear" href="#">
 					Home
-				</a>
-				<a className="hover:text-sky-300 transition-all duration-400 ease-linear" href="#">
-					About
-				</a>
-				<a className="hover:text-sky-300 transition-all duration-400 ease-linear" href="#">
-					Contact
 				</a>
 			</div>
 		</nav>
