3bc96d44839d4b97daa1e0692ba65302920f10fc
Prabin1025y <acharyaprabin1025y@gmail.com>
2024-07-28 22:53:37 +0545

    removed strict

--- backend/index.js
+++ backend/index.js
@@ -130,8 +130,7 @@ app.post("/login", async (req, res) => {
                 res.cookie('token', token, {
                     httpOnly: true,
                     secure: true, // Ensures the cookie is sent only over HTTPS
-                    strict: 'None',
-                    path: "/"
+                    path: '/'
                 });
                 console.log("Cookies should be formed", req.cookies);
                 res.send({ userfound: true, error: false, fullname: user.fullname, userid: user._id });
